198710559203e80488cf4ecaadeb3d86c68ec2be
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2024-04-26 20:04:54 -0300

    Atualização script

--- Script/Script.js
+++ Script/Script.js
@@ -1,6 +1,21 @@
+let chateu = 180;
+let sauvignon = 320;
+let bolani = 150;
+let perignon = 180;
+let mazzeo = 280;
+let alion = 1400;
+
+
+
+
 function cupom() {
     cupom = document.getElementById('cupom').value;
     if (cupom = 'FIAP2024') {
         alert('Cupom Valido!\nVocê conseguiu 10% de desconto!');
     }; 
+};
+
+function adicionar() {
+    vinhoBranco1 = document.getElementById('chateu').value;0
+    alert('Vinho adicionado com sucesso');    
 };
