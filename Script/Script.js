6e75b593b9eccf40ceddd1e5c6017a0a37486361
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2024-04-26 21:42:38 -0300

    Correcao_do_link_do_footer

--- Script/Script.js
+++ Script/Script.js
@@ -5,6 +5,13 @@ let perignon = 180;
 let mazzeo = 280;
 let alion = 1400;
 
+let qtdchateu = 0;
+let qtdsauvignon = 0;
+let qtdbolani = 0;
+let qtdperignon = 0;
+let qtdmazzeo = 0;
+let qtdalion = 0;
+
 
 
 
@@ -18,4 +25,4 @@ function cupom() {
 function adicionar() {
     vinhoBranco1 = document.getElementById('chateu').value;0
     alert('Vinho adicionado com sucesso');    
-};
+};1
