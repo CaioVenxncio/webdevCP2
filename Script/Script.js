e9c93e1e30bafbcf35eb0a28e8377e4462c9766c
Tiago Morais Neto <128254567+dev-tiago-neto@users.noreply.github.com>
2024-04-30 22:10:46 -0300

    Update Script.js

--- Script/Script.js
+++ Script/Script.js
@@ -134,76 +134,8 @@ function adicionar6() {
 
 function adicionarCarrinho() {
     alert('Vinho adicionado ao carrinho!');
-}/*
-function adicionar() {
-    var qtdvinho1 = parseInt(document.getElementById('vinho1').value);
-    var qtdvinho2 = parseInt(document.getElementById('vinho2').value);
-    var qtdvinho3 = parseInt(document.getElementById('vinho3').value);
-    var qtdvinho3 = parseInt(document.getElementById('vinho3').value);
-    var qtdvinho4 = parseInt(document.getElementById('vinho4').value);
-    var qtdvinho5 = parseInt(document.getElementById('vinho5').value);
-    var qtdvinho6 = parseInt(document.getElementById('vinho6').value);
-    
-    if (qtdvinho1!=='' || qtdvinho1!==0 ) {
-        if (qtdvinho1=='') {
-            resultado1.innerHTML = 0; 
-        } else {
-            resultado1.innerHTML = ` R$${qtdvinho1*180},00`; 
-            valorTotal += (qtdvinho1*180);
-            valor.innerHTML = `R$${valorTotal},00`;
-        }
+}
 
-    }
-    if (qtdvinho2!=='' || qtdvinho2!==0) {
-        if (qtdvinho2 =='') {
-            resultado2.innerHTML = 0; 
-        } else {
-            resultado2.innerHTML = `R$${qtdvinho2*320},00`;
-            valorTotal += (qtdvinho2*320);
-            valor.innerHTML = `R$${valorTotal},00`;
-        }
-
-    }
-     if (qtdvinho3!==''|| qtdvinho3!==0) {
-        if (qtdvinho3 =='') {
-            resultado3.innerHTML = 0; 
-        } else {
-                resultado3.innerHTML = `R$${qtdvinho3*150},00`;
-                valorTotal += (qtdvinho3*150);
-            valor.innerHTML = `R$${valorTotal},00`;
-        }
-    }
-    if (qtdvinho4!==''||qtdvinho4!==0) {
-        if (qtdvinho4 =='') {
-            resultado4.innerHTML = 0; 
-        } else {
-            resultado4.innerHTML = `R$${qtdvinho4*180},00`;
-            valorTotal += (qtdvinho4*180);
-            valor.innerHTML = `R$${valorTotal},00`;
-        }
-
-    }
-    if (qtdvinho5!==''||qtdvinho5!==0) {
-        if (qtdvinho5 =='') {
-            resultado5.innerHTML = 0; 
-        } else {
-            resultado5.innerHTML = `R$${qtdvinho5*280},00`;
-            valorTotal += (qtdvinho5*280);
-            valor.innerHTML = `R$${valorTotal},00`;
-        }
-
-
-    }
-    if (qtdvinho6!==''||qtdvinho6!==0) {
-        resultado6.innerHTML = `R$${qtdvinho6*1400},00`;
-        valorTotal += (qtdvinho6*1400);
-        valor.innerHTML = `R$${valorTotal},00`;
-    } else {
-        resultado6.innerHTML = '0';
-    }
-    
-
-};*/
 function cadastro() {
     var nomeCadastro = document.getElementById('nome').value;
     var emailCadastro = document.getElementById('email').value;
